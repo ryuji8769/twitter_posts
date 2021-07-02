18e6fdbec0ed3c163b3330cc4d526ec5b15e0a3f
ruiji <abcdabcd0517@icloud.com>
2021-07-02 22:21:22 +0900

    投稿機能

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,3 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//初回のみモーダルをすぐ出す判定。flagがモーダル表示のstart_open後に代入される
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  get 'posts/:id' => 'posts#show'
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  
+  get '/' => 'homes#top'
+  get 'about' => 'homes#about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
